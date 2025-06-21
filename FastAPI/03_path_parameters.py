f7d05e985fff2ce97ddd776fe068cb9ec111442f
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-06-21 20:26:33 +0500

    Query Parameters

--- FastAPI/03_path_parameters.py
+++ FastAPI/03_path_parameters.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 app = FastAPI()
@@ -10,9 +10,25 @@ def load_data():
 
 # path parameters
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id:str):
+# path function usage
+def view_patient(patient_id:str=Path(..., description='Patient id in db', example='P011')):
     data = load_data()
     for patient in data:
         if patient['patient_id'] == patient_id:
             return patient
     raise HTTPException(status_code=404, detail='Record Not Found')
+
+# query parameters
+@app.get('/patients_sorted')
+def patients_sorted(sort_by:str=Query(..., description='Sort on age, admission_date'), order:str=Query('asc', description='Sort by asc or desc')):
+    data = load_data()
+    valid_fields = ['age', 'admission_date']
+    order_values = ['asc', 'desc']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail='Invalid field, choose from age, admission_date')
+    if order not in order_values:
+        raise HTTPException(status_code=400, detail='Invalid field, choose from asc, desc')
+    order_by = False if order=='asc' else True
+    sorted_data = sorted(data, key=lambda x:x.get(sort_by, 0), reverse=order_by)
+    return sorted_data
