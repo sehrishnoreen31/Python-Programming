b76e17f5949a90b9f4c6c4effac231ac14819a32
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-06-30 14:43:45 +0500

    Qr code

--- QR_Code/qr_code_generation.py
+++ QR_Code/qr_code_generation.py
@@ -1,5 +1,4 @@
 import qrcode
-import qrcode.constants
 
 # Text to QR Code
 qr_1 = qrcode.make('This is a qr code')
@@ -17,5 +16,18 @@ qr.add_data('This code is generated with QRCode class')
 qr.make(fit=True)
 qr_2 = qr.make_image(fill_color=(230, 100, 255), back_color='white')
 qr_2.save('qr_2.png')
+
+
+# # another qrcode
+# qr_code = qrcode.QRCode(
+#     version=1,
+#     error_correction=qrcode.constants.ERROR_CORRECT_L,
+#     box_size=8,
+#     border=5
+# )
+# qr_code.add_data('A qr code')
+# qr_code.make(fit=True)
+# qr_3 = qr_code.make_image(fill_color=(255,255,230), back_color='white')
+# qr_3.save('qr_3.png')
 
 
